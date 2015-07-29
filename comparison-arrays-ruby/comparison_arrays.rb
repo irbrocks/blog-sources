e70d5cbcd6c293301400b4f08407a0839062e2e7
Angel M de Miguel <angelmm@redborder.net>
2015-07-29 09:48:20 +0200

    First version of comparison_arrays script for the article

---
+++ comparison-arrays-ruby/comparison_arrays.rb
@@ -0,0 +1,9 @@
+#!/user/bin/env ruby
+# 
+# This script is a practical example from:
+#
+#   http://irb.rocks/comparison-arrays-ruby/
+#
+# I recommend you to read the article to complement this script :)
+#
+
