cd0e7d71be34524334e42c07e205d8ea18435a28
Ángel M <angelmm@redborder.net>
2016-04-19 16:46:06 +0200

    Update hash_value to dig

--- reduce-nil-verifications-nested-hashes/hash_value.rb
+++ reduce-nil-verifications-nested-hashes/hash_value.rb
@@ -4,12 +4,12 @@
 # Example:
 # h = { a: { b: { c: 1 }}}
 #
-# hash_value h, :a, :b              # { c: 1 }
-# hash_value h, :a, :b, :c          # 1
-# hash_value h, :a, :b, :d          # nil
-# hash_value h, :a, :d              # nil
-# hash_value h, :a, :d, :c          # nil
-# hash_value h, :a, :b, :c, :d      # nil
+# dig h, :a, :b              # { c: 1 }
+# dig h, :a, :b, :c          # 1
+# dig h, :a, :b, :d          # nil
+# dig h, :a, :d              # nil
+# dig h, :a, :d, :c          # nil
+# dig h, :a, :b, :c, :d      # nil
 #
 # == Parameters:
 # hash::
@@ -20,11 +20,11 @@
 # == Returns:
 # Value of the key in the hash or nil
 #
-def hash_value(hash, *keys)
+def dig(hash, *keys)
   if hash[keys.first] && keys.size == 1
     return hash[keys.first]
   elsif hash[keys.first] && hash[keys.first].is_a?(Hash)
-    hash_value(hash[keys.first], *keys[1..keys.size-1])
+    dig(hash[keys.first], *keys[1..keys.size-1])
   else
     return nil
   end
